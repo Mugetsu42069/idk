4894539348ffd73ed20cc746cab1aab52d5dd5a1
BipinpreetSingh <sbipinpreet@gmail.com>
2023-09-20 16:58:09 +0530

    taskdone

--- Bipintask4.py
+++ Bipintask4.py
@@ -1,67 +1,69 @@
-import os
 import json
+import os
+import glob
 
-def list_json_files(directory):
-    json_files = []
-    for root, _, files in os.walk(directory):
-        for file in files:
-            if file.endswith('.json'):
-                json_files.append(os.path.join(root, file))
-    return json_files
+# Specify the directory where the JSON files are located
+directory_path = '/home/bipinpreetsingh/Pythontasks/dsl-schema-main/schemas/'
 
-def json_to_avro_schema(json_data):
-    avro_schema = {
-        "type": "record",
-        "name": "Example",
-        "fields": []
+# Use glob to get a list of JSON files in the directory
+json_file_paths = glob.glob(os.path.join(directory_path, '*.json'))
+
+# Define the output directory where text files will be saved
+output_directory = '/home/bipinpreetsingh/Pythontasks/dsl-schema-main/output/'
+
+# Create the output directory if it doesn't exist
+os.makedirs(output_directory, exist_ok=True)
+
+# Define the allowed destination type map
+allowedDestTypeMap = {
+    "String": "string",
+    "LowCardinality(String)": "string",
+    "DateTime64(0, 'UTC')": "integer",
+    "DateTime": "integer",
+    "UInt8": "integer",
+    "UInt32": "integer",
+    "UInt64": "integer",
+    "Int64": "integer"
+}
+
+# Create a list of allowed destination types
+allowedDestTypes = list(allowedDestTypeMap.keys())
+
+# Iterate over each JSON file
+for json_file_path in json_file_paths:
+    # Initialize the JSON schema for each file
+    jsonSchema = {
+        "type": "object",
+        "properties": {}
     }
-    
-    if isinstance(json_data, dict):
-        for key, value in json_data.items():
-            field = {
-                "name": key,
-                "type": ["null", "string"] if value is None else "string" if isinstance(value, str) else "int" if isinstance(value, int) else "double" if isinstance(value, float) else "boolean" if isinstance(value, bool) else json_to_avro_schema(value)
-            }
-            avro_schema["fields"].append(field)
-    elif isinstance(json_data, list):
-        if json_data:
-            avro_schema["fields"].append({
-                "name": "items",
-                "type": ["null", json_to_avro_schema(json_data[0])]
-            })
 
-    return avro_schema
+    # Read the JSON file
+    with open(json_file_path, 'r', encoding='utf-8') as file:
+        schema = json.load(file)
 
-def main(directory):
-    json_files = list_json_files(directory)
+    # Concatenate the audit_logs and table_creation arrays
+    items = schema.get("audit_logs", []) + schema.get("table_creation", [])
 
-    if not json_files:
-        print("No JSON files found in the specified directory.")
-        return
-
-    avro_schema_dir = '/home/bipinpreetsingh/Pythontasks/dsl-schema-main/avro_schemas/'  # Directory to save Avro schema files
-
-    # Create the output directory if it doesn't exist
-    if not os.path.exists(avro_schema_dir):
-        os.makedirs(avro_schema_dir)
-
-    for json_filename in json_files:
-        with open(json_filename, 'r') as json_file:
-            json_data = json.load(json_file)
+    # Iterate through each item
+    for item in items:
+        dest_datatype = item["dest_datatype"]
+        dest_column = item["dest_column"]
         
-        # Infer the Avro schema from JSON data
-        avro_schema = json_to_avro_schema(json_data)
+        # Check if the dest_datatype is in allowedDestTypes
+        if dest_datatype in allowedDestTypes:
+            jsonSchema["properties"][dest_column] = {
+                "type": allowedDestTypeMap[dest_datatype]
+            }
 
-        # Create the Avro schema file name
-        avro_schema_filename = os.path.splitext(os.path.basename(json_filename))[0] + '.avsc'
-        avro_schema_file_path = os.path.join(avro_schema_dir, avro_schema_filename)
+    # Convert the resulting JSON schema to a JSON string
+    jsonSchemaString = json.dumps(jsonSchema, indent=2)
 
-        # Save the Avro schema to a file
-        with open(avro_schema_file_path, 'w') as avro_schema_file:
-            json.dump(avro_schema, avro_schema_file, indent=4)
+    # Create the output text file path
+    output_file_path = os.path.join(output_directory, os.path.basename(json_file_path).replace('.json', '.txt'))
 
-        print(f"Created Avro schema: {avro_schema_file_path}")
+    # Write the JSON schema to the output text file
+    with open(output_file_path, 'w', encoding='utf-8') as output_file:
+        output_file.write(f"JSON Schema for {json_file_path}:\n")
+        output_file.write(jsonSchemaString)
 
-if __name__ == '__main__':
-    input_directory = '/home/bipinpreetsingh/Pythontasks/dsl-schema-main/schemas/'  # Replace with the path to your directory
-    main(input_directory)
+    print(f"JSON Schema for {json_file_path} saved to {output_file_path}.")
